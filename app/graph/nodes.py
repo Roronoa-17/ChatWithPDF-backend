5676e9629a7c445a246a3b8fe122ee3bc128f0f6
Priyesh H. Gawali <thepriyesh17@gmail.com>
2026-09-17 17:21:30 +0530

    added thread id or namespace associated with each upload

--- app/graph/nodes.py
+++ app/graph/nodes.py
@@ -1,15 +1,25 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableConfig
 from app.graph.state import GraphState
 from app.services.vectorstore import get_vectorstore
+from app.core.config import settings
 
-llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
+llm = ChatGoogleGenerativeAI(model=settings.GEMINI_MODEL_NAME)
 output_parser = StrOutputParser()
 
-def retrieve_node(state: GraphState):
-    vectorstore = get_vectorstore()
-    docs = vectorstore.similarity_search(state["question"], k=3)
+def retrieve_node(state: GraphState, config: RunnableConfig):
+    # Extract the thread_id from LnagGraph's configuration
+    active_namespace = config.get("configurable", {}).get("thread_id")
+    print(f"DEBUG: Retriever is searching inside Namespace: {active_namespace}")
+    if not active_namespace:
+        raise ValueError("Critical Error: No thread_id provided to retriever!")
+    # Passing namespace into vectorstore initialization
+    vectorstore = get_vectorstore(namespace=active_namespace)
     
+    # PDF specific chunks retrieval
+    docs = vectorstore.similarity_search(state["question"], k=3)
+    print(f"DEBUG: Retriever found {len(docs)} documents!")
     context = "\n\n".join([
         f"[Page {doc.metadata.get('page', 0) + 1}]: {doc.page_content}"
         for doc in docs
